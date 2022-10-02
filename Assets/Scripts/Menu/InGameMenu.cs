3386e7220b11996d9fec5645ad3ededab7415841
Madhav Kapa <madhavkapa16@gmail.com>
2022-10-02 16:08:07 -0400

    added audio

--- Assets/Scripts/Menu/InGameMenu.cs
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -6,10 +6,12 @@ public class InGameMenu : MonoBehaviour
     private GameObject _camera;
     [SerializeField] private GameObject inGameMenu;
     public bool gameisPaused;
+    public AudioSource[] audios;
 
     private void Awake()
     {
         if (Camera.main != null) _camera = Camera.main.gameObject;
+        audios = FindObjectsOfType<AudioSource>();
     }
 
     void Update()
@@ -25,12 +27,20 @@ public class InGameMenu : MonoBehaviour
         if(gameisPaused)
         {
             Time.timeScale = 0f;
+            for (int i = 0; i < audios.Length; i++)
+            {
+                audios[i].Pause();
+            }
             inGameMenu.SetActive(gameisPaused);
             _camera.GetComponent<Navigation>().enabled = false;
         }
         else 
         {
             Time.timeScale = 1;
+            for (int i = 0; i < audios.Length; i++)
+            {
+                audios[i].Play();
+            }
             inGameMenu.SetActive(gameisPaused);
             _camera.GetComponent<Navigation>().enabled = true;
         }
@@ -42,10 +52,14 @@ public class InGameMenu : MonoBehaviour
         Time.timeScale = 1;
         inGameMenu.SetActive(gameisPaused);
         _camera.GetComponent<Navigation>().enabled = true;
+        for (int i = 0; i < audios.Length; i++)
+        {
+            audios[i].Play();
+        }
     }
     // Start is called before the first frame update
     public static void BackToMenu()
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Menu",LoadSceneMode.Single);
     }
 }
