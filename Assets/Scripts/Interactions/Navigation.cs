c1b21fb0679186111cb873608bbf885920ed96e8
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-10-02 13:17:47 -0400

    added camera reset on epoch change capability

--- Assets/Scripts/Interactions/Navigation.cs
+++ Assets/Scripts/Interactions/Navigation.cs
@@ -6,12 +6,27 @@ using UnityEngine;
 public class Navigation : MonoBehaviour {
 	[SerializeField]
 	private float rotationseconds = 0.5f;
+
+	private Vector3 startingPosition;
+
+	private Quaternion startingRotation;
+
 	private bool _isRotating;
+
+	void Awake() {
+		startingPosition = transform.position;
+		startingRotation = transform.rotation;
+	}
+
 	void Update() {
 		if( GetLeft() )
 			Turn(Vector2.left);
 		else if( GetRight() )
 			Turn(Vector2.right);
+	}
+
+	public void Restart(Entity entity) {
+		transform.SetPositionAndRotation(startingPosition, startingRotation);
 	}
 
 	private bool GetLeft() => Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
@@ -24,7 +39,7 @@ public class Navigation : MonoBehaviour {
 			return;
 
 		var eulerAngles = transform.eulerAngles;
-		float offset = Mathf.Sign(direction.y) * 90;
+		float offset = Mathf.Sign(direction.x) * 90;
 
 		Quaternion desiredRotQ = Quaternion.Euler(eulerAngles.x, eulerAngles.y + offset, eulerAngles.z);
 		StartCoroutine(SmoothRotate(desiredRotQ));
