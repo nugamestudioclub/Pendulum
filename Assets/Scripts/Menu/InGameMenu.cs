d703c51803b8a386fe51bf763b0e36e03c9fc554
Madhav Kapa <madhavkapa16@gmail.com>
2022-10-02 14:37:19 -0400

    Pause works now :)

--- Assets/Scripts/Menu/InGameMenu.cs
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -1,38 +1,47 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _inGameMenu;
-    public static bool gameIsPaused;
+    private GameObject _camera;
+    [SerializeField] private GameObject inGameMenu;
+    public bool gameisPaused;
+
+    private void Awake()
+    {
+        if (Camera.main != null) _camera = Camera.main.gameObject;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            gameIsPaused = !gameIsPaused;
+            gameisPaused = !gameisPaused;
             PauseGame();
         }
     }
     void PauseGame ()
     {
-        if(gameIsPaused)
+        if(gameisPaused)
         {
             Time.timeScale = 0f;
-            _inGameMenu.SetActive(gameIsPaused);
+            inGameMenu.SetActive(gameisPaused);
+            _camera.GetComponent<Navigation>().enabled = false;
         }
         else 
         {
             Time.timeScale = 1;
-            _inGameMenu.SetActive(gameIsPaused);
+            inGameMenu.SetActive(gameisPaused);
+            _camera.GetComponent<Navigation>().enabled = true;
         }
     }
     
     public void ResumeGame()
     {
-        gameIsPaused = false;
+        gameisPaused = false;
         Time.timeScale = 1;
-        _inGameMenu.SetActive(gameIsPaused);
+        inGameMenu.SetActive(gameisPaused);
+        _camera.GetComponent<Navigation>().enabled = true;
     }
     // Start is called before the first frame update
     public static void BackToMenu()
