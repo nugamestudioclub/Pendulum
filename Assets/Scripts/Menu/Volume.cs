69ce8dfa4fdfea17d60003bb4a7d6cec794bae4c
Madhav Kapa <madhavkapa16@gmail.com>
2022-10-02 19:27:13 -0400

    made volume a playerpref

--- Assets/Scripts/Menu/Volume.cs
+++ Assets/Scripts/Menu/Volume.cs
@@ -3,20 +3,28 @@ using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
-  [SerializeField] private Slider mainSlider;
+    [SerializeField] private Slider mainSlider;
 
-  public void Start()
-  {
-    //Adds a listener to the main slider and invokes a method when the value changes.
-    mainSlider.onValueChanged.AddListener(delegate {VolumeChange(); });
-    mainSlider.value = 0.5f;
-    AudioListener.volume = 0.5f;
-  }
+    public void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        mainSlider.onValueChanged.AddListener(delegate { VolumeChange(); });
+        if (PlayerPrefs.GetFloat("volume") == 0)
+        {
+            PlayerPrefs.SetFloat("volume", 0.5f);
+        }
+        else
+        {
+            var volume = PlayerPrefs.GetFloat("volume");
+            mainSlider.value = volume;
+            AudioListener.volume = volume;
+        }
+    }
 
-  // Invoked when the value of the slider changes.
-  public void VolumeChange()
-  {
-    AudioListener.volume = mainSlider.value;
-  }
-  
+    // Invoked when the value of the slider changes.
+    public void VolumeChange()
+    {
+        AudioListener.volume = mainSlider.value;
+        PlayerPrefs.SetFloat("volume", mainSlider.value);
+    }
 }
