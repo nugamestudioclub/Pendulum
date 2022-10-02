4a75a06c6941c25f0748a6f8242ecf328e0b3d9c
sjkobori <sjkobori@gmail.com>
2022-10-02 16:03:50 -0400

    added clockface prefab, and clockhand script

---
+++ Assets/Scripts/Interactions/Clockhand.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clockhand : ToggleProp
+{
+    public int Ticks => 12;
+
+    [SerializeField]
+    private int length;
+    public int Length => length;
+    private CapsuleCollider col;
+
+    private void Awake()
+    {
+        col = GetComponent<CapsuleCollider>();
+        col.radius = Length; 
+    }
+    protected override int NumStates => Ticks;
+
+    protected override void EnterState(int stateNum)
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            -(float)stateNum / Ticks * 360);
+    }
+
+    protected override void Activate()
+    {
+        if (GameManager.IsTimeFrozen)
+        {
+            Toggle();
+        }
+    }
+}
