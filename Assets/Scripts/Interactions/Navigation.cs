05d26b91b012b0bd82fc8b228bd28728058cff6b
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-10-02 12:58:50 -0400

    stopped camera turning when game is frozen

--- Assets/Scripts/Interactions/Navigation.cs
+++ Assets/Scripts/Interactions/Navigation.cs
@@ -3,50 +3,44 @@ using UnityEngine;
 
 // ReSharper disable CompareOfFloatsByEqualityOperator
 
-public class Navigation : MonoBehaviour
-{
-    [SerializeField] private float rotationseconds = 0.5f;
-    private bool _isRotating;
-    void Update()
-    {
-        //rotate the object depending on the keys pressed
-        if (Input.anyKeyDown)
-        {
-            Turn();
-        }
-    }
-    private void Turn()
-    {
-        if (Input.GetKeyDown(KeyCode.A) && !_isRotating)
-        {
-            var eulerAngles = transform.eulerAngles;
-            Quaternion desiredRotQ = Quaternion.Euler(eulerAngles.x, eulerAngles.y - 90, eulerAngles.z);
-            StartCoroutine(SmoothRotate(desiredRotQ));
-        }
+public class Navigation : MonoBehaviour {
+	[SerializeField]
+	private float rotationseconds = 0.5f;
+	private bool _isRotating;
+	void Update() {
+		if( GetLeft() )
+			Turn(Vector2.left);
+		else if( GetRight() )
+			Turn(Vector2.right);
+	}
 
-        if (Input.GetKeyDown(KeyCode.D) && !_isRotating)
-        {
-            var eulerAngles = transform.eulerAngles;
-            Quaternion desiredRotQ = Quaternion.Euler(eulerAngles.x, eulerAngles.y + 90, eulerAngles.z);
-            StartCoroutine(SmoothRotate(desiredRotQ));
-        }
-    }
+	private bool GetLeft() => Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
 
-    IEnumerator SmoothRotate(Quaternion desiredRotQ)
-    { 
-        
-        if (!_isRotating)
-        {
-            _isRotating = true;
-            float currentRotatonSecs = 0;
-            while (currentRotatonSecs < rotationseconds)
-            {
-                currentRotatonSecs += Time.deltaTime;
-                transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Mathf.Clamp(currentRotatonSecs / rotationseconds,0f,1f));
-                yield return null;
-            }
-            _isRotating = false;
-        }
-        
-    }
+	private bool GetRight() => Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+
+	//rotate the object depending on the keys pressed
+	private void Turn(Vector2 direction) {
+		if( _isRotating )
+			return;
+
+		var eulerAngles = transform.eulerAngles;
+		float offset = Mathf.Sign(direction.y) * 90;
+
+		Quaternion desiredRotQ = Quaternion.Euler(eulerAngles.x, eulerAngles.y + offset, eulerAngles.z);
+		StartCoroutine(SmoothRotate(desiredRotQ));
+	}
+
+	IEnumerator SmoothRotate(Quaternion desiredRotQ) {
+		if( !_isRotating ) {
+			_isRotating = true;
+			float currentRotatonSecs = 0;
+			while( currentRotatonSecs < rotationseconds ) {
+				currentRotatonSecs += Time.deltaTime;
+				transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Mathf.Clamp(currentRotatonSecs / rotationseconds, 0f, 1f));
+				yield return null;
+			}
+			_isRotating = false;
+		}
+
+	}
 }
