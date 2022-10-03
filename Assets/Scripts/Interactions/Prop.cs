4c404dbc87f5271b87d25e2d0237a75104bd879b
Madhav Kapa <madhavkapa16@gmail.com>
2022-10-02 23:47:47 -0400

    added soundeffect code in prop

--- Assets/Scripts/Interactions/Prop.cs
+++ Assets/Scripts/Interactions/Prop.cs
@@ -1,15 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public abstract class Prop : MonoBehaviour
 {
+    public AudioClip sound;
     protected abstract void Activate();
+
     private void OnMouseDown()
     {
+        AudioSource audiosc = gameObject.GetComponent<AudioSource>();
         Debug.Log($"{name} was clicked");
+        if (audiosc  == null)
+        {
+            audiosc = gameObject.AddComponent<AudioSource>();
+        }
         
+        audiosc.clip = sound;
+        audiosc.Play();
         Activate();
     }
-   
 }
