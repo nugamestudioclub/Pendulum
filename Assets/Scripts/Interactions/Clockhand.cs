d6077e0884827d4681623475a27620453fc6d3d0
sjkobori <sjkobori@gmail.com>
2022-10-03 16:27:52 -0400

    added clock hands to bg clock and tick them both in time

--- Assets/Scripts/Interactions/Clockhand.cs
+++ Assets/Scripts/Interactions/Clockhand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,5 +35,10 @@ public class Clockhand : ToggleProp
         {
             Toggle();
         }
+    }
+
+    public void Tick()
+    {
+        Toggle();
     }
 }
--- Assets/Scripts/Ticking.cs
+++ Assets/Scripts/Ticking.cs
@@ -10,7 +10,14 @@ public class Ticking : MonoBehaviour
     private AudioClip resetClip;
     private AudioSource source;
 
-
+    [SerializeField]
+    private Clockhand hourHand;
+    [SerializeField]
+    private Clockhand minuteHand;
+    [SerializeField]
+    private Clockhand hourHandInspect;
+    [SerializeField]
+    private Clockhand minuteHandInspect;
     private void Awake()
     {
         source = GetComponent<AudioSource>();
@@ -22,10 +29,14 @@ public class Ticking : MonoBehaviour
             float currentTime = GameManager.TimeElapsed;
             if (Mathf.Abs(currentTime - GameManager.TickLength) < 0.1f)
             {
+                hourHand.Tick();
+                hourHandInspect.Tick();
                 source.PlayOneShot(resetClip);
             }
             else if (Mathf.Abs(currentTime - (int)currentTime) < 0.1f)
             {
+                minuteHand.Tick();
+                minuteHandInspect.Tick();
                 source.PlayOneShot(tickClip);
             }
             //Debug.Log($"Current time: {currentTime}, {(int)currentTime}");
