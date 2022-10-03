13043a25210864f0d84807b0be88c96986f02fe7
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-10-02 20:07:06 -0400

    added ability to inspect objects

---
+++ Assets/Scripts/Interactions/Inspector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Inspector : Prop {
+	[SerializeField]
+	private InspectionTarget target;
+
+	protected override void Activate() {
+		if( !target.IsInspecting )
+			target.Inspect();
+	}
+}
